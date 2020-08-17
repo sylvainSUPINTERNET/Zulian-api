2a273777daee61c2df72d2c41a25e4e38f4a208a
sylvain joly <sylvain.joly00@orange.fr>
2020-08-17 21:24:24 +0200

    [UPDATE] postgres startup

--- src/main/java/org/zulian/ws/Server.java
+++ src/main/java/org/zulian/ws/Server.java
@@ -8,33 +8,35 @@ import javax.websocket.server.ServerEndpoint;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-@ServerEndpoint("/chat/{username}")
+// uuid -> build from user details
+// email + pass ?
+@ServerEndpoint("/api/{uuid}")
 @ApplicationScoped
 public class Server {
 
     Map<String, Session> sessions = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(Session session, @PathParam("username") String username) {
+    public void onOpen(Session session, @PathParam("uuid") String username) {
         sessions.put(username, session);
         broadcast("User " + username + " joined");
         System.out.println("user has joined : " + username);
     }
 
     @OnClose
-    public void onClose(Session session, @PathParam("username") String username) {
+    public void onClose(Session session, @PathParam("uuid") String username) {
         sessions.remove(username);
         broadcast("User " + username + " left");
     }
 
     @OnError
-    public void onError(Session session, @PathParam("username") String username, Throwable throwable) {
+    public void onError(Session session, @PathParam("uuid") String username, Throwable throwable) {
         sessions.remove(username);
         broadcast("User " + username + " left on error: " + throwable);
     }
 
     @OnMessage
-    public void onMessage(String message, @PathParam("username") String username) {
+    public void onMessage(String message, @PathParam("uuid") String username) {
         broadcast(">> " + username + ": " + message);
     }
 
