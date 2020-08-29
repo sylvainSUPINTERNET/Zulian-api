3636f993648207bc6760c44af3e198853ad4ed8f
sylvain joly <sylvain.joly00@orange.fr>
2020-08-29 03:01:59 +0200

    [UPDATE] exp JWT

--- src/main/java/org/zulian/token/GenerateToken.java
+++ src/main/java/org/zulian/token/GenerateToken.java
@@ -8,6 +8,7 @@ import org.eclipse.microprofile.jwt.Claims;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashSet;
 
 
@@ -17,11 +18,16 @@ public class GenerateToken {
 
     @GET
     public String getToken(){
+
+        long nowMillis = System.currentTimeMillis();
+        long exp = nowMillis + 86400000; // 24h
+
         JwtClaimsBuilder tokenClaim = Jwt.claims();
         tokenClaim.issuer("https://quarkus.io/using-jwt-rbac")
                 .upn("jdoe@quarkus.io")
                 .groups(new HashSet<>(Arrays.asList("User", "Admin")))
                 .claim(Claims.birthdate.name(), "2001-07-13")
+                .expiresAt(exp)
                 .sign();
 
         return tokenClaim.json();
